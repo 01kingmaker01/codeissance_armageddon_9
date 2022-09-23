8e8bb65bd2f7de6dea701aba7e9f223f669dadc0
Sameep <sameepsawant10@gmail.com>
2022-09-23 15:07:13 +0530

    passport js done

---
+++ pages/api/google/callback.js
@@ -0,0 +1,23 @@
+import passport from 'passport'
+import '../../../lib/passport'
+import nookies from 'nookies'
+
+export default async function (req, res, next) {
+  passport.authenticate('google', (err, user, info) => {
+    if (err || !user) {
+      return res.redirect(`http://localhost:3000/?a=auth_fail`)
+    }
+
+    //set the cookies.
+    // set the token in cookies.
+    nookies.set({ res }, 'token', info.token, {
+      httpOnly: true,
+      domain: process.env.SERVER_DOMAIN || undefined,
+      maxAge: 60 * 60 * 24 * 7,
+      sameSite: true,
+      path: '/',
+    })
+
+    res.redirect(`http://localhost:3000`)
+  })(req, res, next)
+}
