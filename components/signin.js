6a1ef5874d84c928a95d4f937693f1ee630e32c5
Sameep <sameepsawant10@gmail.com>
2022-09-23 16:47:26 +0530

    google sign in completed

--- components/signin.js
+++ components/signin.js
@@ -46,7 +46,7 @@ export default ({
     {
       iconImageSrc: googleIconImageSrc,
       text: 'Sign In With Google',
-      callback: '/api/google',
+      url: '/api/google',
     },
   ],
 }) => (
@@ -73,9 +73,6 @@ export default ({
               </SocialButton>
             ))}
           </SocialButtonsContainer>
-          <a href="/api/google" tw="w-32 p-2 border-2 border-gray-200">
-            SignIn
-          </a>
           <DividerTextContainer>
             <DividerText>Or Sign in with your e-mail</DividerText>
           </DividerTextContainer>
